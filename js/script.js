5dc632dea8b833e518ac0d282bf66c7a8a52589b
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-08 18:26:49 +0000

    Done with work!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function sumNumbers() {
+  const number = parseInt(document.getElementById('numberInput').value);
+
+  let result = 0;
+
+  for (let i = 1; i <= number; i++) {
+    result += i;
+  }
+
+  document.getElementById("summationResult").innerHTML = "<p>Sum of numbers from 1 to " + number + " = " + result + "</p>";
+}
